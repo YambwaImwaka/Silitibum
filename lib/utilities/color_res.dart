01c8c5b395286eced1949a5afb47843c459b5cac
Yambwa Imwaka <ybeatzzm@gmail.com>
2026-07-14 07:42:16 +0200

    rebuiod

--- lib/utilities/color_res.dart
+++ lib/utilities/color_res.dart
@@ -3,33 +3,19 @@ import 'package:flutter/material.dart';
 class ColorRes {
   static const Color blackPure = Color(0xFF000000);
   static const Color whitePure = Color(0xFFFFFFFF);
-
-  // Primary Brand Colors
-  static const Color themeGradient1 = Color(0xFFFF8A3D); // Warm Orange
-  static const Color themeGradient2 = Color(0xFFFF5F1F); // Deep Burnt Orange
-  static const Color themeAccentSolid = Color(0xFFFF6B2C); // Main Brand Orange
-
-  // Background
+  static const Color themeGradient1 = Color(0xFFD97ACB);
+  static const Color themeGradient2 = Color(0xFFC35CFE);
+  static const Color themeAccentSolid = Color(0xFFB754F9);
   static const Color themeColor = Color(0xFF15161A);
-
-  // Text
   static const Color textDarkGrey = Color(0xFF454545);
   static const Color textLightGrey = Color(0xFF8B8B8B);
-
-  // Accent Colors
-  static const Color orange = Color(0xFFFF6B2C);
-  static const Color green = Color(0xFF00A651);
-  static const Color green1 = Color(0xFF39D353);
-  static const Color likeRed = Color(0xFFFF4D57);
-
-  // Story Gradient
-  static const Color textStoryBgGradient2 = Color(0xFFFFA24A);
-
-  // Buttons
+  static const Color orange = Color(0xFFFF7A19);
+  static const Color green = Color(0xFF009821);
+  static const Color green1 = Color(0xFF34D948);
+  static const Color likeRed = Color(0xFFFF5751);
+  static const Color textStoryBgGradient2 = Color(0xFFFF5757);
   static const Color blueFollow = Color(0xFF3E8BFF);
-  static const Color battleProgressColor = Color(0xFFFFA000);
-
-  // Background Greys
+  static const Color battleProgressColor = Color(0xFF2CC3FF);
   static const Color bgLightGrey = Color(0xFFF9F9F9);
   static const Color bgGrey = Color(0xFFF0F0F0);
   static const Color bgMediumGrey = Color(0xFFF5F5F5);
